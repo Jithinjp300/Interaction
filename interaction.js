29168500727c20a6e6c43e3aef905ccc2ef14532
jithin jp <102979085+Jithinjp300@users.noreply.github.com>
2024-01-18 13:20:20 +0530

    Add files via upload

---
+++ interaction.js
@@ -0,0 +1,15 @@
+function firstchange(){
+    document.getElementById("1stbox").style.width="400px";
+    document.getElementById("2ndbox").style.width="100px";
+    document.getElementById("3rdbox").style.width="100px";
+ }
+ function secondchange(){
+    document.getElementById("2ndbox").style.width="400px";
+        document.getElementById("3rdbox").style.width="100px";
+    document.getElementById("1stbox").style.width="100px";
+ }
+ function thirdchange(){
+    document.getElementById("3rdbox").style.width="400px";
+    document.getElementById("2ndbox").style.width="100px";
+    document.getElementById("1stbox").style.width="100px";
+ }
